20686724beb6e25921616c90ffff943662612e87
Dmitry <dmitry.maslov.almeza@gmail.com>
2021-06-30 20:47:25 +0300

    Домашка 3 багфикс

--- lib/fight_club_images.dart
+++ lib/fight_club_images.dart
@@ -1,6 +1,6 @@
 class FightClubImages {
   FightClubImages._();
 
-  static const enemyAvatar = "assets/icons/enemy-avatar.png";
-  static const youAvatar = "assets/icons/you-avatar.png";
+  static const enemyAvatar = "assets/images/enemy-avatar.png";
+  static const youAvatar = "assets/images/you-avatar.png";
 }
